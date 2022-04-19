7ca0c211fd5e0ce75d5263226022b00c1f76a8bb
Tiana Bettinson <tiana.bettinson@gmail.com>
2022-04-19 08:56:33 -0400

    Created session cookies for logged-in user

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,10 @@
 class MainController < ApplicationController
     def index
-        flash[:notice] = "Logged in successfully"
-        flash[:alert] = "Invalid email or password"
+        # flash[:notice] = "Logged in successfully"
+        # flash[:alert] = "Invalid email or password"
+
+        if session[:user_id]
+            @user = User.find(session[:user_id])
+        end
     end
 end
