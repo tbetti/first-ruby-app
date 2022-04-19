47893f2a6688ce2ffb7ef4c1c8018176eb730f52
Tiana Bettinson <tiana.bettinson@gmail.com>
2022-04-19 08:31:05 -0400

    Added registrations#create to post User data

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,20 @@ class RegistrationsController < ApplicationController
     def new
         @user = User.new #instance variable
     end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            redirect_to root_path, notice: "Successfully created account"
+        else 
+            render :new
+        end
+    end
+
+    private
+
+    def user_params
+        # user is only allowed to set what is permitted (especially useful when there are admin permissions)
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
 end
