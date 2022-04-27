af84873ef65a5bc04d9bc24385315f8c50bb88d5
Tiana Bettinson <tiana.bettinson@gmail.com>
2022-04-27 13:16:23 -0400

    Created a way for user to reset password through email

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -6,10 +6,34 @@ class PasswordResetsController < ApplicationController
         @user = User.find_by(email: params[:email])
 
         if @user.present?
-            #send email
+            # send email
             PasswordMailer.with(user: @user).reset.deliver_later
         end
 
         redirect_to root_path, notice: "If an account with that email was found, we have sent a link to reset your password"
+    end
+
+    def edit
+        # look up user by token and ensure token is not expired
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+        
+    rescue ActiveSupport::MessageVerifier::InvalidSignature
+        redirect_to sign_in_path, alert: "Your password reset token has expired. Please try again" 
+    end
+
+    def update
+        @user = User.find_signed!(params[:token], purpose: "password_reset")
+
+        if @user.update(password_params)
+            redirect_to sign_in_path, notice: "Password reset successfully"
+        else
+            render :edit
+        end
+    end
+
+    private
+
+    def password_params
+        params.require(:user).permit(:password, :password_confirmation)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,10 @@ Rails.application.routes.draw do
   # to access password
   get "password", to: "passwords#edit", as: :edit_password
   patch "password", to: "passwords#update"
+
+  # if user forgets password
+  get "password/reset", to: "password_resets#new"
+  post "password/reset", to: "password_resets#create"
+  get "password/reset/edit", to: "password_resets#edit"
+  patch "password/reset/edit", to: "password_resets#update"
 end
